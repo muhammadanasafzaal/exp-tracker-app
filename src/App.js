325f4e1688a010e3735b82b31fa2de365b896fe7
Muhammad Anas <anaskhan.all95@gmail.com>
2020-06-25 13:01:07 +0500

    expense tracker app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+/* import React from 'react';
+import Child from './Child'
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+      <div >
+        <Child></Child>
+      </div>
+    
+  );
+}
+
+export default App;
+
+
+ */
+
+import React from 'react';
+import './App.css';
+import Child from './Child';
+import {TransactionProvider} from './transContext';
+
+function App() {
+  return (
+    <div>
+      <Child></Child>
     </div>
   );
 }
---
+++ src/transReducer.js
@@ -0,0 +1,23 @@
+ const TransactionReducer = ((state, action)=>{
+    switch(action.type){
+        case "ADD_TRANSACTION": {
+            return [action.payload, ...state]
+        }
+        default:
+            return state;
+    }
+})
+
+export default TransactionReducer; 
+
+/* const TransactionReducer =  ((state, action)=>{
+    switch(action.type){
+        case "ADD_TRANSACTION": {
+            return [action.payload, ...state]
+        } 
+        default:
+            return state;
+    }
+})
+
+export default TransactionReducer; */
